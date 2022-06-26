d21319e7213fd565a79af930da0fbd6e99e11a4f
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-06-26 16:47:27 +0600

    Dotenv file setup done

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = express();
-const port = 3000;
+dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,6 +11,6 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
 });
